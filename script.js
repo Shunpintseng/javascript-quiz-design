67428e8faa52bd0f8c6f86a0d59dfc0547663bed
shunpin tseng <shunpin@gmail.com>
2020-09-16 20:54:08 -0700

    codeUpdate

--- script.js
+++ script.js
@@ -10,24 +10,26 @@
 
 // when timer runs out or all questions are answered, prompt to page for user initial and score to local storage
 
-//set timer 
-var timeEl = document.querySelector(".time");
-var mainEL = document.getElementById("main");
-var timeLeft = 10;
-var start = document.getElementById("start");
-start.addEventListener ("click", startCountDown)
+// set variables, I started with DOM method before learn about Jquery but can change in next revision 
+var timeEl = document.getElementById("timer");
+var timeLeft = 60; //total test time 60 seconds
+var start = document.getElementById("start"); //assign var to start button
+start.addEventListener ("click", startCountDown) //add click event to kick off startCountDown function
 var timeInterval
 
+//Add function to start the count down after "start" button is clicked 
 function startCountDown(){
 timeInterval = setInterval(countDown,1000);
 countDown()
 start.style.display="none"
 questionSection.style.display="inline-block"
 answerChoices.style.display="inline"
+answerSection.style.display="block"
 currentQues =1;
 question1();
-
 }
+
+
 
 function countDown(){
           timeEl.textContent = "Quiz end in " + timeLeft + " seconds"
@@ -41,54 +43,57 @@ function sendMessage(){
     alert ("Time is Up")
 }
 
+//setting up variables and functions for quizs and answers
+
 var questionSection = document.getElementById("quiz");
 var answerChoices = document.getElementById("choices");
 var answerBtns = document.querySelectorAll(".btn-secondary");
 var answerSection = document.getElementById("answer")
 var answer;
-
+var testResult = document.getElementById('testResult')
 
 
 
 function question1 (){
     questionSection.textContent = "Inside which HTML element do we put the JavaScript?";
-    answerBtns[0].textContent = "<script>";
-    answerBtns[1].textContent = "<javascript>";
-    answerBtns[2].textContent = "<js>";
-    answerBtns[3].textContent = "<scripting>"
+    answerBtns[0].textContent = "A. <script>";
+    answerBtns[1].textContent = "B. <javascript>";
+    answerBtns[2].textContent = "C. <js>";
+    answerBtns[3].textContent = "D. <scripting>";
     answer = 0;
     
     
 }
 
 function question2 (){
-    questionSection.textContent = "what color is the sky";
-    answerBtns[0].textContent = "green";
-    answerBtns[1].textContent = "red";
-    answerBtns[2].textContent = "blue";
-    answerBtns[3].textContent = "toast"
-    answer = 2;
+    questionSection.textContent = "Where is the correct place to insert a JavaScript?";
+    answerBtns[0].textContent = "A. The <head> section";
+    answerBtns[1].textContent = "B. The <body> section";
+    answerBtns[2].textContent = "C. The <title> section";
+    answerBtns[3].textContent = "D. The <footer> section";
+    answer = 1;
 }
 
 function question3 (){
-    questionSection.textContent = "what coloxxxxxy";
-    answerBtns[0].textContent = "green";
-    answerBtns[1].textContent = "red";
-    answerBtns[2].textContent = "blue";
-    answerBtns[3].textContent = "toast"
-    answer = 2;
+    questionSection.textContent = 'What is the correct syntax for referring to an external script called "xxx.js"?';
+    answerBtns[0].textContent = 'A. <script name="xxx.js">';
+    answerBtns[1].textContent = 'B. <script src="xxx.js">';
+    answerBtns[2].textContent = 'C. <script href="xxx.js">';
+    answerBtns[3].textContent = 'D. <script ./asset/xxx.js>';
+    answer = 1;
 }
 
 function question4 (){
-    questionSection.textContent = "whsssxxxxssssccfdddis the sky";
-    answerBtns[0].textContent = "green";
-    answerBtns[1].textContent = "red";
-    answerBtns[2].textContent = "blue";
-    answerBtns[3].textContent = "toast"
-    answer = 2;
+    questionSection.textContent = "How do you write 'Hello World' in an alert box?";
+    answerBtns[0].textContent = 'A. msg("Hello World")';
+    answerBtns[1].textContent = 'B. msgBox("Hello World")';
+    answerBtns[2].textContent = 'C. alertBox("Hello World")';
+    answerBtns[3].textContent = 'D. alert("Hello World")';
+    answer = 3;
 }
 
 
+//
 answerBtns[0].addEventListener("click",function(){
     checkAnswer(answer === 0)
 })
@@ -102,13 +107,13 @@ answerBtns[3].addEventListener("click",function(){
     checkAnswer(answer === 3)
 })
 
-var numCorrectAns
-var numWrongAns
-var currentQues
+var numCorrectAns=0;
+var numWrongAns=0;
+var currentQues;
 
 function checkAnswer(isItCorrect){
     if(isItCorrect === true){
-        answerChoices.textcontent = "Correct!"
+        answerSection.textContent = "Correct!"
         numCorrectAns++
     }else{
         answerSection.textContent = "Wrong!"
@@ -130,7 +135,20 @@ function checkAnswer(isItCorrect){
     }
     if (currentQues===5){
         alert("finished")
-
-    }
-    
+        clearInterval(timeInterval);
+        showScore()
+    }  
 }
+
+var form =  document.querySelector(".form")
+
+function showScore(){
+    start.style.display="none"
+    questionSection.style.display="none"
+    answerChoices.style.display="none"
+    answerSection.style.display="none"
+    form.style.display = "block"
+    testResult.style.display = "block"
+    testResult.textContent="Your score is "+(numCorrectAns/4)*100 +"%"
+    }
+
